6f3b79ccae73a284603607c4550d9b2cbd0f72e8
ayushkhandelwal18 <khandelwalayush976@gmail.com>
2026-02-08 11:06:37 +0530

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ dotenv.config();
 connectDatabase();
 
 
-const server=app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
-})
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
